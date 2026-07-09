bb1be135a11de1b9810320e2043f4b2012c8e0a6
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2026-07-09 17:53:41 -0600

    Created data asset for storing weapon-related data.

---
+++ Source/FPS/Data/WeaponData.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "WeaponData.generated.h"
+
+
+UCLASS()
+class FPS_API UWeaponData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, Category="FPS|Weapon Data|Weapons")
+	TMap<FGameplayTag, FName> GripPoints;
+};
